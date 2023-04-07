0ad612b72d495aa924d84cefaa9f18c642519a3f
Mehedi-Hasan27 <muhammad.mehedi.hasan2001@gmail.com>
2023-04-07 12:27:04 +0600

    week 1

---
+++ New folder/Module-1/output.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+int rahim,karim;
+
+scanf("%d %d",&rahim,&karim);
+printf("%d %d", rahim,karim);
+return 0;
+
+}
---
+++ New folder/Module-3/for_loop1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=3;i=i+1)
+    {
+        printf("Mehedi Hasan\n");
+    }
+    return 0;
+}
